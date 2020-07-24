944bf33479df3c325d38358ca715eee1fcb58637
Elena Martínez Marín <elemartinezmarin@protonmail.com>
2020-07-24 11:56:36 +0200

    Create Form and CardViewer Components

--- src/components/card/Form.js
+++ src/components/card/Form.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Collapsible from './Collapsible';
 import '../../stylesheets/Form.scss';
 import Arrow from '../../images/noun_astrology.svg';
 
@@ -6,55 +7,42 @@ class Form extends React.Component {
   render() {
     return (
       <form className="page__container__form js-form" action="#" method="GET">
-        <div className="page__container__form__icon">
-          <div className="page__container__form__icon__title">
-            <i className="far fa-object-ungroup"></i>
-            <h2>Diseña</h2>
-          </div>
-          <button className="js-angleDown1 rot" type="button">
-            <img src={Arrow} alt="arrow" />
-          </button>
-        </div>
-        <div className="page__container__form__design js-design">
-          <h3>Colores</h3>
-          <div className="page__container__form__design__colorPalette">
-            <label htmlFor="drawing">
-              <input type="radio" id="drawing" name="drawing" className="cold-palette" defaultChecked />
-              <div className="cold">
-                <span className="square square1"></span>
-                <span className="square square2"></span>
-                <span className="square square3"></span>
-              </div>
-            </label>
-            <label htmlFor="drawing2">
-              <input type="radio" id="drawing2" name="drawing" className="hot-palette" />
-              <div className="hot">
-                <span className="square square1"></span>
-                <span className="square square2"></span>
-                <span className="square square3"></span>
-              </div>
-            </label>
-            <label htmlFor="drawing3">
-              <input type="radio" id="drawing3" name="drawing" className="mix-palette" />
-              <div className="mix">
-                <span className="square square1"></span>
-                <span className="square square2"></span>
-                <span className="square square3"></span>
-              </div>
-            </label>
-          </div>
-        </div>
 
-        <div className="page__container__form__icon">
-          <div className="page__container__form__icon__title">
-            <i className="far fa-keyboard"></i>
-            <h2>Rellena</h2>
+        {/* CONTENEDOR 1 */}
+        <Collapsible iconClass="far fa-object-ungroup" title="Diseña">
+          <div className="page__container__form__design js-design">
+            <h3>Colores</h3>
+            <div className="page__container__form__design__colorPalette">
+              <label htmlFor="drawing">
+                <input type="radio" id="drawing" name="drawing" className="cold-palette" defaultChecked />
+                <div className="cold">
+                  <span className="square square1"></span>
+                  <span className="square square2"></span>
+                  <span className="square square3"></span>
+                </div>
+              </label>
+              <label htmlFor="drawing2">
+                <input type="radio" id="drawing2" name="drawing" className="hot-palette" />
+                <div className="hot">
+                  <span className="square square1"></span>
+                  <span className="square square2"></span>
+                  <span className="square square3"></span>
+                </div>
+              </label>
+              <label htmlFor="drawing3">
+                <input type="radio" id="drawing3" name="drawing" className="mix-palette" />
+                <div className="mix">
+                  <span className="square square1"></span>
+                  <span className="square square2"></span>
+                  <span className="square square3"></span>
+                </div>
+              </label>
+            </div>
           </div>
-          <button className="js-angleDown2" type="button">
-            <img src={Arrow} />
-          </button>
-        </div>
-        <fieldset className="page__container__form__fill js-formData hidden">
+        </Collapsible>
+        
+        <Collapsible iconClass="far fa-keyboard" title="Rellena">
+        <fieldset className="page__container__form__fill js-formData ">
           <label htmlFor="firstName"> Nombre completo </label>
           <input id="firstName" type="text" className="js-userName" placeholder="Ej: Sally Jill" required />
 
@@ -84,31 +72,26 @@ class Form extends React.Component {
           <label htmlFor="github"> Github</label>
           <input id="github" type="text" className="js-github" placeholder="Ej: @sally-hill" />
         </fieldset>
-
-        <div className="page__container__form__icon">
-          <div className="page__container__form__icon__title">
-            <i className="fas fa-share-alt"></i>
-            <h2>Comparte</h2>
+        </Collapsible>
+        <Collapsible iconClass="fas fa-share-alt" title="Comparte">        
+          <div className="page__container__form__share js-share ">
+            <i className="far fa-address-card"></i>
+            <input className="create" type="submit" value="crear tarjeta" disabled />
           </div>
-          <button className="js-angleDown3" type="button">
-            <img src={Arrow} alt="arrow" />
-          </button>
-        </div>
-        <div className="page__container__form__share js-share hidden">
-          <i className="far fa-address-card"></i>
-          <input className="create" type="submit" value="crear tarjeta" disabled />
-        </div>
-        <div className="create__text js-createText hidden">
-          <h4>La tarjeta ha sido creada:</h4>
-          <a className="js-url url" title="Ir a la tarjeta" target="_blank"></a>
-          <a
-            className="twitter-share-button"
-            href="https://twitter.com/intent/tweet?text=Hello%20world"
-            data-size="large"
-          >
-            <i className="fab fa-twitter"></i>Compartir en twitter
-          </a>
-        </div>
+          <div className="create__text js-createText hidden">
+            <h4>La tarjeta ha sido creada:</h4>
+            <a className="js-url url" title="Ir a la tarjeta" target="_blank"></a>
+            <a
+              className="twitter-share-button"
+              href="https://twitter.com/intent/tweet?text=Hello%20world"
+              data-size="large"
+            >
+              <i className="fab fa-twitter"></i>Compartir en twitter
+            </a>
+          </div> 
+        </Collapsible>
+        
+
       </form>
     );
   }
