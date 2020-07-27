4e5f640be7930fac562c243f576734b1a1fb707e
Elena Martínez Marín <elemartinezmarin@protonmail.com>
2020-07-27 17:56:17 +0200

    run docs

---
+++ docs/precache-manifest.52b6db2bef96efafc4f2bb5d0a1399af.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97f34119388b6509c4573c5a382d1601",
+    "url": "./index.html"
+  },
+  {
+    "revision": "89e69c032fe97696d483",
+    "url": "./static/css/main.7d36147a.chunk.css"
+  },
+  {
+    "revision": "4bb0584b6b2246a589a1",
+    "url": "./static/js/2.c805d930.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.c805d930.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89e69c032fe97696d483",
+    "url": "./static/js/main.ea3a9f05.chunk.js"
+  },
+  {
+    "revision": "d398f17f5fe35014928b",
+    "url": "./static/js/runtime-main.735673b0.js"
+  },
+  {
+    "revision": "1a8879607d5084632496fa7d85f1e63c",
+    "url": "./static/media/background-b.1a887960.jpg"
+  },
+  {
+    "revision": "da8176df23d52b49160b2f3d0dc83fe1",
+    "url": "./static/media/card-fool2.da8176df.png"
+  },
+  {
+    "revision": "c9cce8438138e02a157abf37a9803aad",
+    "url": "./static/media/card-heart.c9cce843.png"
+  },
+  {
+    "revision": "3c0c1452f2f0a9470aa55d208295399b",
+    "url": "./static/media/card-moon6.3c0c1452.png"
+  },
+  {
+    "revision": "7e4a69758212c5f9a5a5b95d342fa79e",
+    "url": "./static/media/constelaciones.7e4a6975.jpeg"
+  },
+  {
+    "revision": "13330e7afd05beef638366f12ac2eee5",
+    "url": "./static/media/crop-3.13330e7a.jpeg"
+  },
+  {
+    "revision": "e923ad9a30f316e779520db938d06e79",
+    "url": "./static/media/logo-awesome-profile-cards.e923ad9a.svg"
+  },
+  {
+    "revision": "f7cbfe7213d4bfda86850ab5bfed7d52",
+    "url": "./static/media/noun_astrology.f7cbfe72.svg"
+  }
+]);
