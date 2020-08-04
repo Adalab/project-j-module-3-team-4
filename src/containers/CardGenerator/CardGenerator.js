cbb39b808b6afa6ea5a3a1717fa36501f05fd6e9
Mercedes <mcarballalfr@gmail.com>
2020-08-04 14:46:58 +0200

    pull

--- src/containers/CardGenerator/CardGenerator.js
+++ src/containers/CardGenerator/CardGenerator.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
-import Card from '../../components/Card/Card';
+import Card from '../../components/card/Card';
 import Form from '../../components/Form/Form';
 import Footer from '../../components/Footer/Footer';
 import './CardGenerator.scss';
