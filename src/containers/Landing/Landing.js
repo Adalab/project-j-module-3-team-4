361d5049edc4c5d933e49318c7da67858404b571
Corina <cborcoci@gmail.com>
2020-08-05 20:33:59 +0200

    Adjust Links Route

--- src/containers/Landing/Landing.js
+++ src/containers/Landing/Landing.js
@@ -1,37 +1,37 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import Footer from "../../components/Footer/Footer";
-import LandingButton from "../../components/LandingButton/LandingButton";
-import LandingSteps from "../../components/LandingSteps/LandingSteps";
-import "./Landing.scss";
-import BrandLogo from "../../images/logo-awesome-profile-cards.svg";
-import CardSelectorList from "../../components/ThemeCards/CardSelectorList/CardSelectorList";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Footer from '../../components/Footer/Footer';
+import LandingButton from '../../components/LandingButton/LandingButton';
+import LandingSteps from '../../components/LandingSteps/LandingSteps';
+import './Landing.scss';
+import BrandLogo from '../../images/logo-awesome-profile-cards.svg';
+import CardSelectorList from '../../components/ThemeCards/CardSelectorList/CardSelectorList';
 
 class Landing extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      theme: "fool",
+      theme: 'fool',
     };
   }
 
   render() {
     return (
       <div>
-        <div className="page index1">
-          <div className="page__box">
+        <div className='page index1'>
+          <div className='page__box'>
             <img
               src={BrandLogo}
-              alt="Awesome Profile Cards Logo"
-              className="page__box__logo"
+              alt='Awesome Profile Cards Logo'
+              className='page__box__logo'
             />
-            <h1 className="page__box__title">Crea tu tarjeta de visita</h1>
-            <p className="page__box__paragraph">
+            <h1 className='page__box__title'>Crea tu tarjeta de visita</h1>
+            <p className='page__box__paragraph'>
               Crea mejores contactos profesionales de forma fácil y cómoda
             </p>
             <LandingSteps />
-            <Link to="/card-generator">
-              <LandingButton isLanding={this.props.isLanding} />
+            <Link to='/card-generator'>
+              <LandingButton />
             </Link>
             <CardSelectorList />
           </div>
